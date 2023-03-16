a7d449c63a6250f88f851e67452507b63164cb81
Raphael Gonçalves <raphaelricardo10@gmail.com>
2023-03-16 22:35:50 +0000

    refactor: added notification snackbar to application

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,15 @@
 import { useSolverApi } from "./hooks/use-solver-api";
+import { NotificationSnackbar } from "./components/notification-snackbar";
 import { SolverResultsDataGrid } from "./components/solver-results-datagrid";
 
 function App(): JSX.Element {
   const solverApiHook = useSolverApi();
-  return <SolverResultsDataGrid rows={solverApiHook.data} />;
+  return (
+    <>
+      <NotificationSnackbar />
+      <SolverResultsDataGrid rows={solverApiHook.data} />
+    </>
+  );
 }
 
 export default App;
