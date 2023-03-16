46a70d3496cf6f4ee74d4c4c36d315ffcd28b3d0
Raphael Gonçalves <raphaelricardo10@gmail.com>
2023-03-16 15:10:22 +0000

    test: refectored tests isolating the api response with spy

--- src/hooks/use-solver-api/hook.test.ts
+++ src/hooks/use-solver-api/hook.test.ts
@@ -1,16 +1,54 @@
-import { renderHook } from "@testing-library/react";
-import { useSolverApi } from "./hook";
+import { renderHook, RenderHookResult, waitFor } from "@testing-library/react";
+import { SolverApiHook, useSolverApi } from "./hook";
+import { SolverResult, SolverService } from "../../services/solver-service";
+import { act } from "react-dom/test-utils";
 
 describe("solver api hook", () => {
-  it("should be initialized with in_progress state", () => {
-    const { result } = renderHook(useSolverApi);
-    expect(result.current.solverResults.status).toBe("in_progress");
+  const mockResult: SolverResult[] = [
+    { number_of_solutions: 2, chessboard_size: 4, execution_time: 0.1 },
+  ];
+  let hook: RenderHookResult<SolverApiHook, {}>;
+  beforeAll(() => {
+    jest.useFakeTimers();
+    jest.spyOn(SolverService, "getResults").mockImplementation(
+      () =>
+        new Promise((resolve, reject) => {
+          setTimeout(
+            () =>
+              resolve({
+                data: mockResult,
+                status: "successful",
+              }),
+            5000
+          );
+        })
+    );
   });
 
-  it("should be completed with successful state", () => {
-    const { result } = renderHook(useSolverApi);
-    setTimeout(() => {
-      expect(result.current.solverResults.status === "successful");
-    }, 2000);
+  afterAll(() => {
+    jest.useRealTimers();
+    jest.restoreAllMocks();
+  });
+
+  beforeEach(() => {
+    hook = renderHook(useSolverApi);
+  });
+
+  afterEach(() => {
+    hook.unmount();
+  });
+
+  it("should be initialized with in_progress state", async () => {
+    expect(hook.result.current.solverResults.status).toBe("in_progress");
+  });
+
+  it("should return the api data", async () => {
+    act(() => {
+      jest.runAllTimers();
+    });
+
+    await waitFor(() => {
+      expect(hook.result.current.solverResults.status).toBe("successful");
+    });
   });
 });
