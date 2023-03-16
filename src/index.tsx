d69739badf4bd0c4c5fd4ff0515779fef343d991
Raphael Gonçalves <raphaelricardo10@gmail.com>
2023-03-16 17:45:47 +0000

    refactor: fixed eslint problems

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./app";
-import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from "./report-web-vitals";
 
 const root = ReactDOM.createRoot(
   document.querySelector("#root") as HTMLElement
