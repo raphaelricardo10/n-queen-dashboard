53238ecf89f4a12b644d8c4002601188a16c9d8f
Raphael Gonçalves <raphaelricardo10@gmail.com>
2023-03-16 18:45:25 +0000

    refactor: removed the react landing page and added the solver results datagrid

--- src/app.tsx
+++ src/app.tsx
@@ -1,25 +1,9 @@
-import logo from "./logo.svg";
-import "./app.css";
+import { useSolverApi } from "./hooks/use-solver-api";
+import { SolverResultsDataGrid } from "./components/solver-results-datagrid/datagrid";
 
 function App(): JSX.Element {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const hook = useSolverApi();
+  return <SolverResultsDataGrid rows={hook.solverResults.data} />;
 }
 
 export default App;
