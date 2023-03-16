f939eddc7d845b0e939e87403ef7b70c3029a803
Raphael Gonçalves <raphaelricardo10@gmail.com>
2023-03-16 18:04:30 +0000

    refactor: handled if the data object is a function for backward compatibility.

--- src/services/solver-service/service.ts
+++ src/services/solver-service/service.ts
@@ -6,8 +6,14 @@ export const SolverService = {
   getResults: async (): Promise<ApiMessage<SolverResult[]>> => {
     try {
       const response = await api.get("/solver-results");
-      return response.data();
+
+      if (typeof response.data === "function") {
+        return response.data();
+      }
+
+      return response.data;
     } catch (error: any) {
+      console.error(error);
       return {
         data: [],
         status: "error",
